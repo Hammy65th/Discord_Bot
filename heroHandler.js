18b7e2ceadce5266e7efa64608778cfe792569c6
Hammy65th <peterkingo93@gmail.com>
2025-04-23 09:36:40 +0200

    V2

--- heroHandler.js
+++ heroHandler.js
@@ -1,18 +1,25 @@
 //399246587882373152 | Hero
 //267308620058853376 | Hammy
+//316234162182815755 | Alpaca
+//236962814239965184 | Aug
 
 module.exports = (client) => {
-  const targetUserId = '399246587882373152';
-
-  const reactionEmojii = '<:hero:1364172567585230869>'
+  const targetUserReactions ={ 
+    '399246587882373152' : '<:yappingyap:1364314235357630564>',  
+    '316234162182815755' : '🥰',
+    '236962814239965184' : '<:Gjob:1364312133273387118>',
+    '378256488751431680' : '<:hero:1364172567585230869>'
+  };
 
   client.on('messageCreate', (message) => {
     if (message.author.bot) return;
 
-    if (message.author.id === targetUserId) {
-      message.react(reactionEmojii)
-      .then(() => console.log(`Reacted to message from ${message.author.tag}`))
-      .catch((error) => console.error('Error reacting to message:', error))
+    const reactionEmoji = targetUserReactions[message.author.id];
+
+    if (reactionEmoji) {
+      message.react(reactionEmoji)
+        .then(() => console.log(`Reacted to message from ${message.author.tag}`))
+        .catch((error) => console.error('Error reacting to message:', error));
     }
   });
-};;
+};
